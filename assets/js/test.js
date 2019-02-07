e2a55325d2c70d986f180a952287370ab774f671
gotdzellal <gotdzellal@gmail.com>
2019-02-07 22:32:25 +0100

    Added proper blocks for the Tags

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,14 +1,14 @@
 
 var elements = document.getElementsByName('tag');
-var kangals = document.getElementsByClassName('kangal');
+var blocks = document.getElementsByClassName('block');
 for (let tag of elements) {
     tag.addEventListener('change', function () {
         var check = this.id.slice(0, this.id.length - 7);
-        for (let kangal of kangals) {
-            if (kangal.classList.contains(check)) {
-                kangal.style.display = 'inline-block';
+        for (let block of blocks) {
+            if (block.classList.contains(check)) {
+                block.style.display = 'inline-block';
             } else {
-                kangal.style.display = 'none';
+                block.style.display = 'none';
             }
         }
     });
